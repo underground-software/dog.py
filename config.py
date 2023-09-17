c9fada43baa9ec2bc167d68b267f73de089a2c99
Joel Savitz <joelsavitz@gmail.com>
2023-09-17 07:29:45 +0300

    config.py: update watchog interval default to 60 seconds
    Signed-off-by: Joel Savitz <joelsavitz@gmail.com>

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 # configuration for dog.py tools
 
 # seconds to wait between each activation of the watchdog thread
-DOGDAEMON_WATCHDOG_INTERVAL_SECS    =  3
+DOGDAEMON_WATCHDOG_INTERVAL_SECS    =  60
 # filename inwhich to store watchdog records
 DOGDAEMON_WATCHDOG_DOGLOG_FILENAME   = 'dog.log'
